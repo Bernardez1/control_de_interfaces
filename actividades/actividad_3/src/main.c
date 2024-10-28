4042f51f755bf8a8b218306c21302645285df7a8
Bernardez1 <166428359+Bernardez1@users.noreply.github.com>
2024-10-28 13:48:14 -0300

    actividad 3
    resolucion de la actividad

--- actividades/actividad_3/src/main.c
+++ actividades/actividad_3/src/main.c
@@ -1,28 +1,42 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-
-/**
- * @brief Programa principal
-*/
 int main(void) {
-	// Inicializo el USB
 	stdio_init_all();
-	// Demora para esperar la conexion
-	sleep_ms(1000);
+	gpio_init(20);
+	gpio_set_dir(20,false);
+	gpio_init(21);
+	gpio_set_dir(21,false);
+	gpio_init(22);
+	gpio_set_dir(22,false);
 
-	// Inicializacion de GPIO con gpio_init()
+	gpio_init(6);
+	gpio_set_dir(6,true);
+	gpio_init(7);
+	gpio_set_dir(7,true);
+	gpio_init(8);
+	gpio_set_dir(8,true);
 
-	// Configuracion de entrada/salida con gpio_set_dir()
 
+	while (1) {
 
-	while (true) {
-
-		// Resolver logica para GPIO20 -> GPIO6
-
-		// Resolver logica para GPIO21 -> GPIO7
-
-		// Resolver logica para GPIO22 -> GPIO8
-
+	if (gpio_get(20)==false){
+	gpio_put(6,true);
 	}
+	else
+	{
+		gpio_put(6,false);
+		sleep_ms(100);
+	}
+	if (gpio_get(21)==false){
+		gpio_put(7, true);
+	if (gpio_get(21)==false _AND gpio_get(7)==true){
+		gpio_put(7,false);
+		}
+		}
+	if (gpio_get(22)==false){
+		gpio_put(8,true);
+	if (gpio_get(22)==false _AND gpio_get(8)==true){
+		gpio_put(8,false);
+		sleep_ms(500);}
+		}
+		}
 	return 0;
 }
